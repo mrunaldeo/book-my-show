0075831fe3eb98e50584cf080b22dad08cc641bb
Mrunal Deo <mrunaldeo16@gmail.com>
2023-12-12 20:54:04 +0530

    Add files via upload

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,1 +1,26 @@
+import React from "react";
+import { Tabs } from "antd";
+// import { useSelector, useDispatch } from "react-redux";
+import PageTitle from "../../components/PageTitle";
+import TheatresList from "./TheatreList";
+import Bookings from "./Bookings";
+// import Bookings from "./Bookings";
+function Profile() {
+  return (
+    <div>
+      <PageTitle title="Profile" />
 
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Bookings" key="1">
+           <Bookings/>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Apply for Theater" key="2">
+           <TheatresList/>
+           {/* <h1>Theatre List</h1> */}
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
+}
+
+export default Profile;
