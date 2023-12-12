c75bca2df17df09f086aa991e55055a3aafcfd8a
Mrunal Deo <mrunaldeo16@gmail.com>
2023-12-12 17:19:00 +0530

    Delete server/config directory

--- server/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.mongo_url)
-
-const connection = mongoose.connection
-
-connection.on('connected',()=>{
-    console.log("Connection Established")
-})
