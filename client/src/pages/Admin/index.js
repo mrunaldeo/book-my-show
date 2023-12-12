fd8aca7c18ac989bdcff076f2b0c4df09bd763c0
Mrunal Deo <mrunaldeo16@gmail.com>
2023-12-12 20:46:23 +0530

    Add files via upload

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,1 +1,25 @@
+import React from "react";
+import PageTitle from "../../components/PageTitle";
+import { Tabs } from "antd";
+ import MoviesList from "./MoviesList";
+import TheatreTable from "./TheatreTable";
 
+function Admin() {
+  return (
+    <div>
+      <PageTitle title="Admin" />
+
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Movies" key="1">
+            <MoviesList/>
+        </Tabs.TabPane>
+
+        <Tabs.TabPane tab="Theatres" key="2">
+            <TheatreTable/>
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
+}
+
+export default Admin;
